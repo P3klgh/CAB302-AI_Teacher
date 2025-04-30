e1d2474ef0b4f4519e55cbb64ce5ed2cdf4f7bd3
Kenneth Lee <n11532386@qut.edu.au>
2025-04-30 17:37:27 +1000

    Test

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,13 +2,10 @@ module com.cab302ai_teacher.ai_teacher {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-    requires org.testng;
     requires org.junit.jupiter.api;
     exports com.cab302ai_teacher;
     exports com.cab302ai_teacher.controller to javafx.fxml;
 
     opens com.cab302ai_teacher to javafx.fxml;
     opens com.cab302ai_teacher.controller to javafx.fxml;
-    exports com.cab302ai_teacher.util to javafx.fxml;
-    opens com.cab302ai_teacher.util to javafx.fxml;
 }
